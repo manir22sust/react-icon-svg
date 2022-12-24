5aef194c2fe9026ae3ee6b667b30a0c69c98e471
Manir Uddin <manir22sust@gmail.com>
2022-12-25 00:38:16 +0100

    svg icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+
+import Globe from "./Globe";
+import ArrowPath from "./ArrowPath";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Globe className="App-logo" />
+        <ArrowPath className="App-logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
